0f66462244f9af301c017abc9b2cff7c01fb3a70
Hans van Arken <hansvanarken@gmail.com>
2016-09-12 13:02:30 +0200

    Added sourcemaps again

--- gulpfile.js
+++ gulpfile.js
@@ -1,55 +1,27 @@
 const gulp = require('gulp');
 const babelify = require('babelify');
-const uglify = require('gulp-uglify');
 const browserify = require('browserify');
 const source = require('vinyl-source-stream');
 const buffer = require('vinyl-buffer');
-const sass = require('gulp-sass');
+const uglify = require('gulp-uglify');
 const sourcemaps = require('gulp-sourcemaps');
-const browserSync = require('browser-sync').create();
 
-gulp.task('scripts', () => {
-  var bundler = browserify({
-    entries: './src/script.es6',
-    debug: true
-  })
-    .transform(babelify, {presets: ['es2015']})
-    .bundle();
+gulp.task('run', function() {
+    var bundler = browserify('./src/script.es6', {debug:true})
+        .transform(babelify, {presets: ['es2015']})
+        .bundle();
 
-  return bundler
-    .pipe(source('bundle.js'))
-    .pipe(buffer())
-    .pipe(sourcemaps.init({loadMaps: true}))
-    .pipe(uglify())
-    .pipe(sourcemaps.write('./'))
-    .pipe(gulp.dest('dist'))
-    .pipe(browserSync.stream());
+    return bundler
+        .pipe(source('script.js'))
+        .pipe(buffer())
+        .pipe(sourcemaps.init({loadMaps: true}))
+        .pipe(uglify())
+        .pipe(sourcemaps.write('./'))
+        .pipe(gulp.dest('dist'));
 });
 
-gulp.task('sass', function () {
-  return gulp.src('./sass/**/*.scss')
-    .pipe(sourcemaps.init())
-    .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
-    .pipe(sourcemaps.write('./'))
-    .pipe(gulp.dest('dist'))
-    .pipe(browserSync.stream());
+gulp.task('watch', function() {
+    gulp.watch('./src/**/*.*', ['run']);
 });
 
-gulp.task('watch', () => {
-  gulp.watch('./src/**/*.*', ['scripts']);
-  gulp.watch('./sass/**/*.scss', ['sass']);
-});
-
-// Static Server + watching scss/html files
-gulp.task('serve', ['sass'], function () {
-
-  browserSync.init({
-    server: "./"
-  });
-
-  gulp.watch('./src/**/*.*', ['scripts']);
-  gulp.watch('./sass/**/*.scss', ['sass']);
-  gulp.watch('./*.html').on('change', browserSync.reload);
-});
-
-gulp.task('default', ['serve', 'sass', 'scripts']);
+gulp.task('default', ['watch', 'run']);
